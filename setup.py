f52dd3c7e59857eacb7ca1e8e872c0c5a8006cec
Laurent Peuch <cortex@worlddomination.be>
2013-01-19 18:20:44 +0100

    [enh] add a setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding:Utf-8 -*-
+
+from setuptools import setup
+
+setup(name='oerpjh',
+      version='0.1',
+      description='Use haml and jinja2 in OpenERP views',
+      author='Laurent Peuch',
+      #long_description='',
+      author_email='cortex@worlddomination.be',
+      url='http://',
+      install_requires=['Hamlish-Jinja'],
+      packages=[],
+      py_modules=['oerpjh'],
+      license= '',
+      scripts=[],
+      keywords='',
+     )
+
