a002ebeac031149f41e51aa8f810672d2be91431
Laurent Peuch <cortex@worlddomination.be>
2013-01-19 18:20:32 +0100

    init, first working version

---
+++ oerpjh.py
@@ -0,0 +1,25 @@
+import os
+import sys
+from jinja2 import Environment, FileSystemLoader
+from hamlish_jinja import HamlishExtension
+
+def modernize():
+    called_from = sys._getframe(1).f_globals["__file__"]
+    module_directory = os.path.split(called_from)[0]
+
+    if not os.path.exists(os.path.join(module_directory, "__openerp__.py")):
+        return
+
+    env = Environment(extensions=[HamlishExtension], loader=FileSystemLoader(module_directory))
+
+    module_settings = eval(open(os.path.join(module_directory, "__openerp__.py")).read())
+    xml_files = list(set(filter(lambda x: x.strip().endswith(".xml"), module_settings.get("init_xml", []) + module_settings.get("data", []) + module_settings.get("update_xml", []))))
+
+    print xml_files
+
+    for xml_file in xml_files:
+        haml_file = xml_file[:-3] + "haml"
+        haml_file_path = os.path.join(module_directory, xml_file[:-3] + "haml")
+        if not os.path.exists(haml_file_path):
+            continue
+        open(os.path.join(module_directory, xml_file), "w").write(env.get_template(haml_file).render())
