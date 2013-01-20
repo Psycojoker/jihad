197a6055cdc1973c04332fa0b95ff19fa5227a3d
Laurent Peuch <cortex@worlddomination.be>
2013-01-20 17:01:03 +0100

    [doc] add licence

--- oerpjh.py
+++ oerpjh.py
@@ -1,3 +1,18 @@
+# This file is part of Openerp-Jinja2-Haml.
+#
+# OpenERP-Jinja2-Haml is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# OpenERP-Jinja2-Haml is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# OpenERP-Jinja2-Haml.  If not, see <http://www.gnu.org/licenses/>.
+
 import os
 import sys
 from jinja2 import Environment, FileSystemLoader
