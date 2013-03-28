29e4b2437b0878fa4e7aa7c91efeedd967d4827d
Laurent Peuch <cortex@worlddomination.be>
2013-03-28 18:00:18 +0100

    [mod] another release

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@
 from setuptools import setup
 
 setup(name='jihad',
-      version='0.1.3',
+      version='0.1.4',
       description='Use haml and jinja2 in OpenERP views',
       author='Laurent Peuch',
       #long_description='',
